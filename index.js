cddf1d494cc3003ad401c2edac1118cf246c768c
Pavel Klochkov <paul.klochkov@gmail.com>
2021-12-28 15:16:22 +0100

    Add some custom routing

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import express from 'express';
+// eslint-disable-next-line import/extensions
+import usersRouter from './users/users.js';
 
 const port = 8000;
 const app = express();
 
+app.all('/hello', (req, res, next) => {
+  console.log(req.cookie);
+  console.log(res.cookie());
+  next();
+});
+
 app.get('/hello', ((req, res) => {
   res.send('Hello from NodeJS Pure server');
 }));
+
+app.use('/users', usersRouter);
 
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Server started at http://localhost:${port}`));
