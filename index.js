e6fcba5eb5a646c05f022f70ae3723c2e7390ca7
Pavel Klochkov <paul.klochkov@gmail.com>
2021-12-28 14:44:42 +0100

    With default routing

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ const host = '127.0.0.1';
 const port = 8000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from NodeJS Pure server');
+  switch (req.method) {
+    case 'GET':
+      switch (req.url) {
+        case '/hello':
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/plain');
+          res.end('Hello from NodeJS Pure server');
+          break;
+        default:
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/plain');
+          res.end('NodeJS Pure server');
+          break;
+      }
+      break;
+  }
 });
 
 // eslint-disable-next-line no-console
