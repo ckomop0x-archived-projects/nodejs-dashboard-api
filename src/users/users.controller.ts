e33dea553f1d88e734f4c2ed2b9a785ab3e7c588
Pavel Klochkov <paul.klochkov@gmail.com>
2022-03-26 12:01:14 +0100

    Add JWT to login

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,6 @@
 import 'reflect-metadata';
+import { sign } from 'jsonwebtoken';
+
 import BaseController from '../common/base.controller';
 import { NextFunction, Response, Request } from 'express';
 import { HttpError } from '../errors/http-error.class';
@@ -8,14 +10,16 @@ import { ILogger } from '../logger/logger.interface';
 import { IUsersController } from './users.controller.interface';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
-import { UsersService } from './users.service';
 import { ValidateMiddleware } from '../common/validate.middleware';
+import { IConfigService } from '../config/config.service.interface';
+import { IUsersService } from './users.service.interface';
 
 @injectable()
 export class UsersController extends BaseController implements IUsersController {
 	constructor(
 		@inject(TYPES.ILogger) private loggerService: ILogger,
-		@inject(TYPES.UsersService) private userService: UsersService,
+		@inject(TYPES.UsersService) private userService: IUsersService,
+		@inject(TYPES.ConfigService) private configService: IConfigService,
 	) {
 		super(loggerService);
 		this.bindRoutes([
@@ -39,24 +43,6 @@ export class UsersController extends BaseController implements IUsersController 
 		]);
 	}
 
-	async login(
-		req: Request<{}, {}, UserLoginDto>,
-		res: Response,
-		next: NextFunction,
-	): Promise<void> {
-		const isValidUser = await this.userService.validateUser(req.body);
-
-		if (!isValidUser) {
-			return next(new HttpError(401, 'Auth error', 'login'));
-		}
-
-		this.ok(res, {});
-	}
-
-	logout(req: Request, res: Response, next: NextFunction): void {
-		this.ok(res, 'Logout');
-	}
-
 	async register(
 		req: Request<{}, {}, UserRegisterDto>,
 		res: Response,
@@ -68,5 +54,44 @@ export class UsersController extends BaseController implements IUsersController 
 		// await newUser.setPassword(req.body.password);
 		// console.log(newUser);
 		this.ok(res, { email: result.email, id: result.id });
+	}
+
+	async login(
+		req: Request<{}, {}, UserLoginDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		const isValidUser = await this.userService.validateUser(req.body);
+
+		if (!isValidUser) {
+			return next(new HttpError(401, 'Auth error', 'login'));
+		}
+
+		const jwt = await this.signJWT(req.body.email, this.configService.get('JWT'));
+
+		this.ok(res, { jwt });
+	}
+
+	logout(req: Request, res: Response, next: NextFunction): void {
+		this.ok(res, 'Logout');
+	}
+
+	private signJWT(email: string, secret: string): Promise<string> {
+		return new Promise<string>((resolve, reject) => {
+			sign(
+				{
+					email,
+					iat: Math.floor(Date.now() / 1000),
+				},
+				secret,
+				{ algorithm: 'HS256' },
+				(err, token) => {
+					if (err) {
+						reject(err);
+					}
+					resolve(token as string);
+				},
+			);
+		});
 	}
 }
