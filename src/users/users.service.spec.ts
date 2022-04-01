459bcf315cfb853cbd8080ad7f935f63e190ce20
Pavel Klochkov <paul.klochkov@gmail.com>
2022-04-01 15:04:39 +0200

    Add unit test

---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,60 @@
+import 'reflect-metadata';
+import { Container } from 'inversify';
+import { IConfigService } from '../config/config.service.interface';
+import { IUsersRepository } from './users.repository.interface';
+import { IUsersService } from './users.service.interface';
+import { TYPES } from '../types';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+import { UserModel } from '@prisma/client';
+
+const ConfigServiceMock: IConfigService = {
+	get: jest.fn(),
+};
+
+const UsersRepositoryMock: IUsersRepository = {
+	find: jest.fn(),
+	create: jest.fn(),
+};
+
+const container = new Container();
+let configService: IConfigService;
+let usersRepository: IUsersRepository;
+let usersService: IUsersService;
+
+beforeAll(() => {
+	container.bind<IUsersService>(TYPES.UsersService).to(UsersService);
+	container.bind<IConfigService>(TYPES.ConfigService).toConstantValue(ConfigServiceMock);
+	container.bind<IUsersRepository>(TYPES.UsersRepository).toConstantValue(UsersRepositoryMock);
+
+	configService = container.get<IConfigService>(TYPES.ConfigService);
+	usersRepository = container.get<IUsersRepository>(TYPES.UsersRepository);
+	usersService = container.get<IUsersService>(TYPES.UsersService);
+});
+
+let createdUser: UserModel | null;
+
+describe('User Service', () => {
+	it('Should create user', async () => {
+		configService.get = jest.fn().mockReturnValueOnce('1');
+		usersRepository.create = jest.fn().mockImplementationOnce(
+			(user: User): UserModel => ({
+				name: user.name,
+				email: user.email,
+				password: user.password,
+				id: 1,
+			}),
+		);
+
+		createdUser = await usersService.createUser({
+			email: 'pavel@pk.me',
+			name: 'Pavel',
+			password: '1P@ssword!',
+		});
+
+		expect(createdUser?.id).toBe(1);
+		expect(createdUser?.password).not.toEqual('1P@ssword!');
+	});
+});
+
+afterAll(() => {});
